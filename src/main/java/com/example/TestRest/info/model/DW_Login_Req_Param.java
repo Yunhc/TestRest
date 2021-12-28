c70320141ea7080d3cd4b336507b68b44f8ae975
Yunhc <yunhc@wavelink.co.kr>
2021-12-28 14:26:28 +0900

    Create DW_Login_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Login_Req_Param.java
@@ -0,0 +1,12 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Login_Req_Param {
+	public String lang;
+	public String userid;
+	public String password;
+}
