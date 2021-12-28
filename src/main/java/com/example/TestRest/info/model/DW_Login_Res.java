4fb9041729f0b87b139619fda4a1f44b3652e3ae
Yunhc <yunhc@wavelink.co.kr>
2021-12-28 14:26:31 +0900

    Create DW_Login_Res.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Login_Res.java
@@ -0,0 +1,26 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class DW_Login_Res {
+	public String code;
+	public String message;
+	public String userid;
+	public String username;
+	public String userpwd;
+	public String plantcd;
+	public String wccode;
+	public String warehouse;
+	public String auth;
+	public String role;
+	public String use_role;
+	public String useflag;
+	public String forklift;
+	public String etc;
+	public String upduser;
+	public String upddate;	
+}
