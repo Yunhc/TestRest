87b638dd82b357f9b6fa46a352517a6eb678e1b2
Yunhc <yunhc@wavelink.co.kr>
2021-12-29 11:34:36 +0900

    Update InfoController.java

--- src/main/java/com/example/TestRest/info/InfoController.java
+++ src/main/java/com/example/TestRest/info/InfoController.java
@@ -1,22 +1,11 @@
 package com.example.TestRest.info;
 
-import java.util.Date;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.TestRest.info.model.User;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
 import lombok.extern.slf4j.Slf4j;
-
 import com.example.TestRest.info.model.City;
 
 @Slf4j
