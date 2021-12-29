88452a45b52c9659b7f023ad0e9901613154b3e7
Yunhc <yunhc@wavelink.co.kr>
2021-12-29 11:34:48 +0900

    Update TestRestApplication.java

--- src/main/java/com/example/TestRest/TestRestApplication.java
+++ src/main/java/com/example/TestRest/TestRestApplication.java
@@ -1,22 +1,10 @@
 package com.example.TestRest;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import com.example.TestRest.info.model.DW_User;
-import com.example.TestRest.info.repository.DW_UserRepository;
-import com.example.TestRest.info.repository.DW_UserSql;
-
-import lombok.extern.slf4j.Slf4j;
 
 
-@Slf4j
 @SpringBootApplication
 //public class TestRestApplication implements CommandLineRunner {
 public class TestRestApplication {
