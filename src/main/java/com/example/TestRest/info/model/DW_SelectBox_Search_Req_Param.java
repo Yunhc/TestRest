93610f6feaaf5ee785ddd5b5f2ee83bd070e0d0e
Yunhc <yunhc@wavelink.co.kr>
2022-01-05 09:37:43 +0900

    Create DW_SelectBox_Search_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_SelectBox_Search_Req_Param.java
@@ -0,0 +1,18 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_SelectBox_Search_Req_Param {
+	public String lang;
+	public String userid;
+	public String plant;
+	public String type1;
+	public String type2;
+	public String type3;
+	public String type4;
+	public String space;
+
+}
