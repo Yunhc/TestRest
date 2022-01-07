b906cb6023840d1df9adac8dd5ddc6b908428802
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-07 16:11:52 +0900

    Create DWT_STC_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_STC_Req_Param.java
@@ -0,0 +1,17 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_STC_Req_Param {
+	public String werks;
+	public String lgort;
+	public String barno;
+	public int qty;
+	public String loc;
+	public String userid;
+	public String force;
+	public String online;
+}
