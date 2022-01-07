05b275650e0052ff39d25246222727908b69755f
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-07 16:11:55 +0900

    Create DWT_STC_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_STC_Res_Param.java
@@ -0,0 +1,17 @@
+
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_STC_Res_Param {
+	public String barno;
+	public String matnr;
+	public int qty;
+	public String werks;
+	public String lgort;
+	public String rowno;
+	public String desc;
+}
