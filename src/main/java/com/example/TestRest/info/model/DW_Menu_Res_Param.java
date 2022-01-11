9f7bf434dd06b290b137c4fcc9b477a51ba0a449
Yunhc <yunhc@wavelink.co.kr>
2022-01-11 09:53:32 +0900

    Create DW_Menu_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Menu_Res_Param.java
@@ -0,0 +1,18 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Menu_Res_Param {
+	public String menuid;
+	public String menuname;
+	public int sortkey;
+	public String useflag;
+	public String etc;
+	public String upduser;
+	public String upddate1;
+	public String upddate2;
+
+}
