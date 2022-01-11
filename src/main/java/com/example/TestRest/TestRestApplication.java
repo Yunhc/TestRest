a92d0f91406fccbb1c81049d9a10dd87529a9c8c
Yunhc <yunhc@wavelink.co.kr>
2022-01-11 09:53:44 +0900

    Update TestRestApplication.java

--- src/main/java/com/example/TestRest/TestRestApplication.java
+++ src/main/java/com/example/TestRest/TestRestApplication.java
@@ -1,28 +1,30 @@
-package com.example.TestRest;
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-@SpringBootApplication
-//public class TestRestApplication implements CommandLineRunner {
-public class TestRestApplication {
-
-//	@Autowired
-//	private JdbcTemplate jdbcTemplate;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(TestRestApplication.class, args);
-	}
-	
-//	@Override
-//	public void run(String... args) throws Exception {
-//		String  sql = "select getdate()";
-//		
-//		List<DW_User> dw_user = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(DW_User.class));
-//		
-//		log.debug("findList query = {}", sql);
+//package com.example.TestRest;
+//
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//
+//@SpringBootApplication
+////public class TestRestApplication implements CommandLineRunner {
+//public class TestRestApplication {
+//
+////	@Autowired
+////	private JdbcTemplate jdbcTemplate;
+//	
+//	public static void main(String[] args) {
+//		SpringApplication.run(TestRestApplication.class, args);
 //	}
+//	
+////	@Override
+////	public void run(String... args) throws Exception {
+////		String  sql = "select getdate()";
+////		
+////		List<DW_User> dw_user = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(DW_User.class));
+////		
+////		log.debug("findList query = {}", sql);
+////	}
+//
+//}
 
-}
+
