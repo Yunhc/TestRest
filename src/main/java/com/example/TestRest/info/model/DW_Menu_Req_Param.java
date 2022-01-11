756ed71df724bc08c8368dc3a12b108454dbb5b8
Yunhc <yunhc@wavelink.co.kr>
2022-01-11 09:53:28 +0900

    Create DW_Menu_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Menu_Req_Param.java
@@ -0,0 +1,14 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Menu_Req_Param {
+	public String lang;
+	public String userid;
+	public String menuid;
+	public String menuname;
+	public String useflag;
+}
