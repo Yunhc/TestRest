b0bd0f218859c6627f341afae0131654ed0ebab9
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-12 15:41:36 +0900

    Create DWT_FG_Receipt_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_FG_Receipt_Res_Param.java
@@ -0,0 +1,19 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_FG_Receipt_Res_Param {
+	public String barno;
+	public int qty;
+	public String meins;
+	public String matnr;	
+	public String maktx;
+	public String lgort;
+	public String proddate;
+	public String status;
+	public String code;
+	public String message;
+}
