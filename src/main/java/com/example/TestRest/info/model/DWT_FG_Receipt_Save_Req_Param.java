967acd970aa7ec862c6af8d8bdba144c3522451c
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-12 17:45:20 +0900

    Create DWT_FG_Receipt_Save_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_FG_Receipt_Save_Req_Param.java
@@ -0,0 +1,13 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_FG_Receipt_Save_Req_Param {
+	public String i_lang;
+	public String i_userid;
+	public String i_werks;
+	public String i_barno;
+}
