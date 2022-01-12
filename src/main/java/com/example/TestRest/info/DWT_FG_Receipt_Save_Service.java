b8b2ddc6ffe736611fb59f044bf016675791dcef
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-12 17:45:17 +0900

    Create DWT_FG_Receipt_Save_Service.java

---
+++ src/main/java/com/example/TestRest/info/DWT_FG_Receipt_Save_Service.java
@@ -0,0 +1,22 @@
+package com.example.TestRest.info;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.TestRest.info.model.DWT_FG_Receipt_Save_Req_Param;
+import com.example.TestRest.info.model.DWT_FG_Receipt_Save_Res_Param;
+import com.example.TestRest.info.repository.DWT_FG_Receipt_Save_Repository;
+
+@Service
+public class DWT_FG_Receipt_Save_Service {
+	private final DWT_FG_Receipt_Save_Repository dw_repository;
+	
+	public DWT_FG_Receipt_Save_Service(DWT_FG_Receipt_Save_Repository dw_repository) {
+		this.dw_repository = dw_repository;
+	}
+	
+	public List<DWT_FG_Receipt_Save_Res_Param> get_List(DWT_FG_Receipt_Save_Req_Param req_param) {
+		return this.dw_repository.findList(req_param);
+	}
+}
