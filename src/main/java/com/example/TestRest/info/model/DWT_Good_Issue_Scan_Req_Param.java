9ab7183a3dba938a43c2595089f5e71d33f2085f
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-17 18:12:46 +0900

    Create DWT_Good_Issue_Scan_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Scan_Req_Param.java
@@ -0,0 +1,16 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_Scan_Req_Param {
+	public String i_lang;
+	public String i_userid;
+	public String i_werks;
+	public String i_vbeln;
+	public String i_barno;
+	public int i_qty;
+	public String i_delflag;
+}
