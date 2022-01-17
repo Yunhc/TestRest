1a6e81d862768e4f068457f6b6dd3b3c0248f41b
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-17 18:12:43 +0900

    Create DWT_Good_Issue_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Res_Param.java
@@ -0,0 +1,24 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_Res_Param {
+	public String matnr;
+	public int posnr;
+	public String orderqtybdl;
+	public String procqty;	
+	public String orderqtypc;
+	public String umrez;
+	public String maktx;
+	public String ztype;
+	public String uepos;
+	public String matnrc;
+	public String kunnr;
+	public String zkunnrnm;
+	public String zshipno;
+	public String code;
+	public String message;
+}
