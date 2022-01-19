9c5f9d703f621c53bb4ecb93e51833cba2f19328
Yunhc <yunhc@wavelink.co.kr>
2022-01-19 11:56:10 +0900

    Update DWT_FG_Receipt_Sql.groovy

--- src/main/java/com/example/TestRest/info/repository/DWT_FG_Receipt_Sql.groovy
+++ src/main/java/com/example/TestRest/info/repository/DWT_FG_Receipt_Sql.groovy
@@ -1,15 +1,27 @@
 package com.example.TestRest.info.repository
 
 import com.example.TestRest.info.model.DWT_FG_Receipt_Req_Param
+import com.example.TestRest.info.model.DWT_FG_Receipt_Save_Req_Param
 
 class DWT_FG_Receipt_Sql {
 	public static String SELECT = """""";
 	
-	//콤보박스 조회
+	//바코드 조회
 	public static SELECT_QUERY(DWT_FG_Receipt_Req_Param req_param)
 	{		
 		SELECT = """""";
 		SELECT += """ EXEC USP_MWMS_FG_RECEIPT_BARCODE_SCAN """
+		SELECT += """  '""" + req_param.i_lang + """'"""
+		SELECT += """, '""" + req_param.i_userid + """'"""
+		SELECT += """, '""" + req_param.i_werks + """'"""
+		SELECT += """, '""" + req_param.i_barno + """'"""
+	}
+	
+	//저장
+	public static SAVE_QUERY(DWT_FG_Receipt_Save_Req_Param req_param)
+	{
+		SELECT = """""";
+		SELECT += """ EXEC USP_MWMS_FG_RECEIPT_SAVE """
 		SELECT += """  '""" + req_param.i_lang + """'"""
 		SELECT += """, '""" + req_param.i_userid + """'"""
 		SELECT += """, '""" + req_param.i_werks + """'"""
