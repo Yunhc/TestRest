ac3f36de63250e4657173dde123309dcaf211ff4
Yunhc <yunhc@wavelink.co.kr>
2022-01-19 11:55:35 +0900

    Update DWT_FG_Receipt_Service.java

--- src/main/java/com/example/TestRest/info/DWT_FG_Receipt_Service.java
+++ src/main/java/com/example/TestRest/info/DWT_FG_Receipt_Service.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import com.example.TestRest.info.model.DWT_FG_Receipt_Req_Param;
 import com.example.TestRest.info.model.DWT_FG_Receipt_Res_Param;
+import com.example.TestRest.info.model.DWT_FG_Receipt_Save_Req_Param;
+import com.example.TestRest.info.model.DWT_FG_Receipt_Save_Res_Param;
 import com.example.TestRest.info.repository.DWT_FG_Receipt_Repository;
 
 @Service
@@ -16,7 +18,13 @@ public class DWT_FG_Receipt_Service {
 		this.dw_repository = dw_repository;
 	}
 	
+	//바코드 조회
 	public List<DWT_FG_Receipt_Res_Param> get_List(DWT_FG_Receipt_Req_Param req_param) {
 		return this.dw_repository.findList(req_param);
+	}
+	
+	//저장
+	public List<DWT_FG_Receipt_Save_Res_Param>save_List(DWT_FG_Receipt_Save_Req_Param req_param) {
+		return this.dw_repository.saveList(req_param);
 	}
 }
