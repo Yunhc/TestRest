004f7f240a4ff8506787083321ac10633cd3a031
Yunhc <yunhc@wavelink.co.kr>
2022-01-19 11:38:51 +0900

    Update DWT_Good_Issue_Sql.groovy

--- src/main/java/com/example/TestRest/info/repository/DWT_Good_Issue_Sql.groovy
+++ src/main/java/com/example/TestRest/info/repository/DWT_Good_Issue_Sql.groovy
@@ -1,6 +1,7 @@
 package com.example.TestRest.info.repository
 
 import com.example.TestRest.info.model.DWT_Good_Issue_Req_Param
+import com.example.TestRest.info.model.DWT_Good_Issue_Scan_Req_Param
 
 class DWT_Good_Issue_Sql {
 	public static String SELECT = """""";
@@ -14,5 +15,19 @@ class DWT_Good_Issue_Sql {
 		SELECT += """, '""" + req_param.i_userid + """'"""
 		SELECT += """, '""" + req_param.i_werks + """'"""
 		SELECT += """, '""" + req_param.i_vbeln + """'"""
+	}
+	
+	//¹ÙÄÚµå ½ºÄµ
+	public static SCAN_QUERY(DWT_Good_Issue_Scan_Req_Param req_param)
+	{
+		SELECT = """""";
+		SELECT += """ EXEC AURUN.BARCODE_AUTEST.DBO.USP_MWMS_GI_BARCODE_SCAN """
+		SELECT += """  '""" + req_param.i_lang + """'"""
+		SELECT += """, '""" + req_param.i_userid + """'"""
+		SELECT += """, '""" + req_param.i_werks + """'"""
+		SELECT += """, '""" + req_param.i_vbeln + """'"""
+		SELECT += """, '""" + req_param.i_barno + """'"""
+		SELECT += """, '""" + req_param.i_qty + """'"""
+		SELECT += """, '""" + req_param.i_delflag + """'"""
 	}
 }
