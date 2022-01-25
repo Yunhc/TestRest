d90161fd6e6b252cebc352cb0c4201570ea1fba8
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-25 17:56:12 +0900

    Create DWT_Good_Issue_DO_Search_Date_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_DO_Search_Date_Req_Param.java
@@ -0,0 +1,13 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_DO_Search_Date_Req_Param {
+	public String i_lang;
+	public String i_userid;
+	public String i_werks;
+	public String i_date;
+}
