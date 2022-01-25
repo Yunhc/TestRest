e0fd284f1167d0ad0d4f86754eb9cc30f4e56ce9
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-25 17:56:15 +0900

    Create DWT_Good_Issue_DO_Search_Date_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_DO_Search_Date_Res_Param.java
@@ -0,0 +1,39 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_DO_Search_Date_Res_Param {
+	public String zshipno;
+	public String vbeln;
+	public int posnr;
+	public String auart;
+	public String wadat;
+	public String bldat;
+	public String matnr;
+	public String lfimg;
+	public String vrkme;
+	public String lgmng;
+	public String meins;
+	public String werks;
+	public String lgort;
+	public String kunnr;
+	public String zkunnrnm;
+	public String kunag;
+	public String zkunagnm;
+	public String zstatus;
+	public String ztype;
+	public String vgbel;
+	public String vgpos;
+	public String rtnqty;
+	public String rtnqmqty;
+	public String rtnflag;
+	public String procqty;
+	public String procflag;
+	public String upddate;
+
+	public String code;
+	public String message;
+}
