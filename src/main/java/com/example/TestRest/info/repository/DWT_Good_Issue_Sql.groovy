89df4ee0d7114f5479d4d585846c56288b6b0beb
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-25 17:56:20 +0900

    Update DWT_Good_Issue_Sql.groovy

--- src/main/java/com/example/TestRest/info/repository/DWT_Good_Issue_Sql.groovy
+++ src/main/java/com/example/TestRest/info/repository/DWT_Good_Issue_Sql.groovy
@@ -2,11 +2,12 @@ package com.example.TestRest.info.repository
 
 import com.example.TestRest.info.model.DWT_Good_Issue_Req_Param
 import com.example.TestRest.info.model.DWT_Good_Issue_Scan_Req_Param
+import com.example.TestRest.info.model.DWT_Good_Issue_DO_Search_Date_Req_Param
 
 class DWT_Good_Issue_Sql {
 	public static String SELECT = """""";
 	
-	//DO 조회
+	//DO 조회 - DO번호
 	public static SELECT_QUERY(DWT_Good_Issue_Req_Param req_param)
 	{		
 		SELECT = """""";
@@ -29,5 +30,16 @@ class DWT_Good_Issue_Sql {
 		SELECT += """, '""" + req_param.i_barno + """'"""
 		SELECT += """, '""" + req_param.i_qty + """'"""
 		SELECT += """, '""" + req_param.i_delflag + """'"""
+	}
+
+	//DO 조회 - 날짜
+	public static SELECT_DO_Date_QUERY(DWT_Good_Issue_DO_Search_Date_Req_Param req_param)
+	{
+		SELECT = """""";
+		SELECT += """ EXEC AURUN.BARCODE_AUTEST.DBO.USP_MWMS_GI_DO_SEARCH_DATE """
+		SELECT += """  '""" + req_param.i_lang + """'"""
+		SELECT += """, '""" + req_param.i_userid + """'"""
+		SELECT += """, '""" + req_param.i_werks + """'"""
+		SELECT += """, '""" + req_param.i_date + """'"""
 	}
 }
