684cf00d9a0faf2179fc85a6c914a7ad7c70eaf1
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-27 16:49:48 +0900

    Create DWT_Good_Issue_Bar_Search_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Bar_Search_Res_Param.java
@@ -0,0 +1,19 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_Bar_Search_Res_Param {
+	public String sel;
+	public String barno;
+	public String qty;
+	public String meins;
+	public String matnr;
+	public String maktx;
+	public String sobdqty;
+
+	public String code;
+	public String message;
+}
