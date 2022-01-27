5742ae46fae07fcb8d330faa63444da652532c92
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-27 16:49:44 +0900

    Create DWT_Good_Issue_Bar_Search_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Bar_Search_Req_Param.java
@@ -0,0 +1,13 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_Bar_Search_Req_Param {
+	public String i_lang;
+	public String i_userid;
+	public String i_werks;
+	public String i_vbeln;
+}
