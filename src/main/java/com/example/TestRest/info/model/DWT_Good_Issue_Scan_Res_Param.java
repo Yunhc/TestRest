f56c63760cb86d76b9e95b2b37a222b2792a0c6a
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-03 17:35:27 +0900

    Update DWT_Good_Issue_Scan_Res_Param.java

--- src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Scan_Res_Param.java
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Scan_Res_Param.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DWT_Good_Issue_Scan_Res_Param {
+	// 바코드 스캔화면에서 호출시 리턴 값
 	public String matnr;
 	public int posnr;
 	public String orderqtybdl;
@@ -19,6 +20,16 @@ public class DWT_Good_Issue_Scan_Res_Param {
 	public String kunnr;
 	public String zkunnrnm;
 	public String zshipno;
+	
 	public String code;
 	public String message;
+	
+	// 바코드 삭제화면에서 호출시 리턴 값 matnr, maktx, code, message는 공통으로 사용
+	public String sel;
+	public String barno;
+	public String qty;
+	public String meins;
+//	public String matnr;
+//	public String maktx;
+	public String sobdqty;
 }
