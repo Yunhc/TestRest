58f187fb241a488ddb09b321d3c4d2c522c54018
Yunhc <yunhc@wavelink.co.kr>
2022-02-04 17:33:38 +0900

    Create DW_LabelHistSearch_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_LabelHistSearch_Res_Param.java
@@ -0,0 +1,30 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_LabelHistSearch_Res_Param {
+	public String WERKS; 
+	public String PLANT;
+	public String SEL;
+	public String BARNO; 
+	public String LGORT;        
+	public String SLNAME;
+	public String MATNR; 
+	public String MAKTX;
+	public String MEINS; 
+	public String QTY;
+	public String PRTCNT;
+	public String EBELN;
+	public String EBELP;
+	public String RSNUM;
+	public String RSPOS;
+	public String AUFNR;
+	public String PLNUM;
+	public String KDAUF;
+	public String KDPOS;
+	public String PRODATE;
+	public String UPDDATE1;
+}
