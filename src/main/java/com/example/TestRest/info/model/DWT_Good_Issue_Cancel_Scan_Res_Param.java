cc847f7e2d8f1f9da58412bfa5e4907c208919be
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-08 18:41:34 +0900

    Create DWT_Good_Issue_Cancel_Scan_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Cancel_Scan_Res_Param.java
@@ -0,0 +1,24 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_Cancel_Scan_Res_Param {
+	public String barno;
+	public String matnr;
+	public String qty;	
+	public String meins;
+	public String vbeln;
+	public String posnr;
+	public String packtype1;
+	public String ZKUNNRNM;
+	public String wadat;
+	public String upddate;
+	public String PROCQTY;
+	public String MAKTX;
+		
+	public String code;
+	public String message;
+}
