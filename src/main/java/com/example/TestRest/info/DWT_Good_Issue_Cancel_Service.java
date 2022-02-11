27a90b22a0cfe470690dc791d1d3beb6f1bcf7e3
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-11 18:27:16 +0900

    Update DWT_Good_Issue_Cancel_Service.java

--- src/main/java/com/example/TestRest/info/DWT_Good_Issue_Cancel_Service.java
+++ src/main/java/com/example/TestRest/info/DWT_Good_Issue_Cancel_Service.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.TestRest.info.model.DWT_Good_Issue_Cancel_Save_Req_Param;
+import com.example.TestRest.info.model.DWT_Good_Issue_Cancel_Save_Res_Param;
 import com.example.TestRest.info.model.DWT_Good_Issue_Cancel_Scan_Req_Param;
 import com.example.TestRest.info.model.DWT_Good_Issue_Cancel_Scan_Res_Param;
 import com.example.TestRest.info.repository.DWT_Good_Issue_Cancel_Repository;
@@ -17,8 +19,13 @@ public class DWT_Good_Issue_Cancel_Service {
 		this.dw_repository = dw_repository;
 	}
 
-	//바코드 스캔
+	//취소할 바코드 스캔
 	public List<DWT_Good_Issue_Cancel_Scan_Res_Param> scan_List(DWT_Good_Issue_Cancel_Scan_Req_Param req_param) {
 		return this.dw_repository.scanList(req_param);
+	}
+
+	//스캔한 바코드 출고 취소
+	public List<DWT_Good_Issue_Cancel_Save_Res_Param> save_List(DWT_Good_Issue_Cancel_Save_Req_Param req_param) {
+		return this.dw_repository.saveList(req_param);
 	}
 }
