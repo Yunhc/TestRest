74f37c394ad30fb3012368c551fc145abcc2f33e
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-11 18:27:24 +0900

    Create DWT_Good_Issue_Cancel_Save_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Cancel_Save_Req_Param.java
@@ -0,0 +1,15 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_Cancel_Save_Req_Param {
+	public String i_lang;
+	public String i_userid;
+	public String i_werks;
+	public String i_barno;
+	public String i_vbeln;
+	public String i_matnr;
+}
