55ab6b66964e3a53faf52b667f65b6cb07228e61
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-11 18:27:32 +0900

    Create DWT_Good_Issue_Cancel_Save_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DWT_Good_Issue_Cancel_Save_Res_Param.java
@@ -0,0 +1,11 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DWT_Good_Issue_Cancel_Save_Res_Param {
+	public String code;
+	public String message;
+}
