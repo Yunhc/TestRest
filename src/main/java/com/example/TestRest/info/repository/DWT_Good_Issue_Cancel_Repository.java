10ff803e2ba55fc4ee1626ed2d9370d24e44b173
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-11 18:27:43 +0900

    Update DWT_Good_Issue_Cancel_Repository.java

--- src/main/java/com/example/TestRest/info/repository/DWT_Good_Issue_Cancel_Repository.java
+++ src/main/java/com/example/TestRest/info/repository/DWT_Good_Issue_Cancel_Repository.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.example.TestRest.info.model.DWT_Good_Issue_Cancel_Save_Req_Param;
+import com.example.TestRest.info.model.DWT_Good_Issue_Cancel_Save_Res_Param;
 import com.example.TestRest.info.model.DWT_Good_Issue_Cancel_Scan_Req_Param;
 import com.example.TestRest.info.model.DWT_Good_Issue_Cancel_Scan_Res_Param;
 
@@ -22,6 +24,7 @@ public class DWT_Good_Issue_Cancel_Repository {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
+	//취소할 바코드 스캔
 	public List<DWT_Good_Issue_Cancel_Scan_Res_Param> scanList(DWT_Good_Issue_Cancel_Scan_Req_Param req_param){
 		
 		DWT_Good_Issue_Cancel_Sql.SELECT_QUERY(req_param );
@@ -29,6 +32,17 @@ public class DWT_Good_Issue_Cancel_Repository {
 
 		
 		List<DWT_Good_Issue_Cancel_Scan_Res_Param> res = this.jdbcTemplate.query(DWT_Good_Issue_Cancel_Sql.SELECT, BeanPropertyRowMapper.newInstance(DWT_Good_Issue_Cancel_Scan_Res_Param.class));
+		return res;
+	}
+
+	//스캔한 바코드 출고취소
+	public List<DWT_Good_Issue_Cancel_Save_Res_Param> saveList(DWT_Good_Issue_Cancel_Save_Req_Param req_param){
+		
+		DWT_Good_Issue_Cancel_Sql.SAVE_QUERY(req_param );
+		log.debug("findList query = {}", DWT_Good_Issue_Cancel_Sql.SELECT);
+
+		
+		List<DWT_Good_Issue_Cancel_Save_Res_Param> res = this.jdbcTemplate.query(DWT_Good_Issue_Cancel_Sql.SELECT, BeanPropertyRowMapper.newInstance(DWT_Good_Issue_Cancel_Save_Res_Param.class));
 		return res;
 	}
 }
