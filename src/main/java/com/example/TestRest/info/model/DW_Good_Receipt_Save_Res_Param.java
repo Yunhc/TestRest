66c9b934ef9cd8d5f589c786b5d014cb40e6a57b
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-14 18:12:33 +0900

    Create DW_Good_Receipt_Save_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Good_Receipt_Save_Res_Param.java
@@ -0,0 +1,14 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Good_Receipt_Save_Res_Param {
+	public String i_lang;
+	public String i_userid;
+	public String i_werks;
+	public String i_vbeln;
+	public String i_procflag;
+}
