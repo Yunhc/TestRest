612d73970ee504faba5ff50b489fa63d057be9cf
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-14 18:12:22 +0900

    Create DW_Good_Receipt_Detail_Search_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Good_Receipt_Detail_Search_Res_Param.java
@@ -0,0 +1,29 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Good_Receipt_Detail_Search_Res_Param {
+	public String ebeln;
+	public String ebelp;
+	public String bsart;
+	public String bedat;	
+	public String lifnr;
+	public String name1;
+	public String bukrs;
+	public String ekgrp;
+	public String bstyp;
+	public String ematn;
+	public String txz01;
+	public String werks;
+	public String lgort;
+	public String menge;
+	public String meins;
+	public String procqty;
+	public String procflag;
+	
+	public String code;
+	public String message;
+}
