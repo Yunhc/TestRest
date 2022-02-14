2d4df3a0f6e6411023e6fb8a2603a6fb6e111855
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-14 18:12:50 +0900

    Create DW_Good_Receipt_Scan_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Good_Receipt_Scan_Req_Param.java
@@ -0,0 +1,18 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Good_Receipt_Scan_Req_Param {
+	public String i_lang;
+	public String i_userid;
+	public String i_werks;
+	public String i_vbeln;
+	public String i_barno;
+	public String i_qty;
+	public String i_delflag;
+	public String i_calltype;
+	public String data;
+}
