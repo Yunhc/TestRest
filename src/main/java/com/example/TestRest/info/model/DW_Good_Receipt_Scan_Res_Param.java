f0b42333972a0e87229e26db61624a7d16954009
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-14 18:12:53 +0900

    Create DW_Good_Receipt_Scan_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Good_Receipt_Scan_Res_Param.java
@@ -0,0 +1,25 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Good_Receipt_Scan_Res_Param {
+	public String matnr;
+	public int posnr;
+	public String orderqtybdl;
+	public String procqty;	
+	public String orderqtypc;
+	public String umrez;
+	public String maktx;
+	public String ztype;
+	public String uepos;
+	public String matnrc;
+	public String kunnr;
+	public String zkunnrnm;
+	public String zshipno;
+	
+	public String code;
+	public String message;
+}
