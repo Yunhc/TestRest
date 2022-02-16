56b2e0419d0ce6dd24fcfd12d91b1c219afbda46
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-16 16:03:03 +0900

    Update DW_Good_Receipt_Detail_Search_Res_Param.java

--- src/main/java/com/example/TestRest/info/model/DW_Good_Receipt_Detail_Search_Res_Param.java
+++ src/main/java/com/example/TestRest/info/model/DW_Good_Receipt_Detail_Search_Res_Param.java
@@ -21,8 +21,25 @@ public class DW_Good_Receipt_Detail_Search_Res_Param {
 	public String lgort;
 	public String menge;
 	public String meins;
+	public String menge2;
 	public String procqty;
+	public String scanqty;
+	public String meins2;
 	public String procflag;
+	public String elikz;
+	public String uebtk;
+	public String uebto;
+	public String maxqty;
+	public String var1;
+	public String var2;
+	public String var3;
+	public String var4;
+	public String var5;
+	public String var6;
+	public String kalsk;
+	public String kdauf;
+	public String kdpos;
+	public String insmk;
 	
 	public String code;
 	public String message;
