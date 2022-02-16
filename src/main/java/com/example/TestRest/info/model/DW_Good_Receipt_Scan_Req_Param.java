4ae033b0479bd652cefabf7e275482b31217250c
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-16 16:03:09 +0900

    Update DW_Good_Receipt_Scan_Req_Param.java

--- src/main/java/com/example/TestRest/info/model/DW_Good_Receipt_Scan_Req_Param.java
+++ src/main/java/com/example/TestRest/info/model/DW_Good_Receipt_Scan_Req_Param.java
@@ -7,12 +7,9 @@ import lombok.Setter;
 @Setter
 public class DW_Good_Receipt_Scan_Req_Param {
 	public String i_lang;
-	public String i_userid;
 	public String i_werks;
-	public String i_vbeln;
+	public String i_userid;
+	public String i_ord_no;
+	public String i_ord_item_no;
 	public String i_barno;
-	public String i_qty;
-	public String i_delflag;
-	public String i_calltype;
-	public String data;
 }
