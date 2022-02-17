16f8803463267744cd6c89762a3def90c444bae0
Yunhc <yunhc@wavelink.co.kr>
2022-02-17 18:04:32 +0900

    Create DW_Auth_Mng_User_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Auth_Mng_User_Res_Param.java
@@ -0,0 +1,19 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Auth_Mng_User_Res_Param {
+	public String sel;
+	public String userid;
+	public String username;
+	public String userpwd;
+	public String plant;
+	public String wc;
+	public String auth;
+	public String useflag;
+	public String upduser;
+	public String upddate;
+}
