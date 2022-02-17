c3964037c3368e1631012337b8ba5ece0ebb2036
Yunhc <yunhc@wavelink.co.kr>
2022-02-17 18:04:30 +0900

    Create DW_Auth_Mng_Service.java

---
+++ src/main/java/com/example/TestRest/info/DW_Auth_Mng_Service.java
@@ -0,0 +1,23 @@
+package com.example.TestRest.info;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.TestRest.info.model.DW_Auth_Mng_User_Res_Param;
+import com.example.TestRest.info.repository.DW_Auth_Mng_Repository;
+
+
+@Service
+public class DW_Auth_Mng_Service {
+	private final DW_Auth_Mng_Repository dw_repository;
+	
+	public DW_Auth_Mng_Service(DW_Auth_Mng_Repository dw_repository) {
+		this.dw_repository = dw_repository;
+	}
+
+	//사용자 조회
+	public List<DW_Auth_Mng_User_Res_Param> findList(String req_param) {
+		return this.dw_repository.findList(req_param);
+	}
+}
