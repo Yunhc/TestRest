5f47f7d8cd7cdced3d1993cdfb75deb006f2e822
Yunhc <yunhc@wavelink.co.kr>
2022-02-17 18:04:39 +0900

    Create DW_Auth_Mng_Sql.groovy

---
+++ src/main/java/com/example/TestRest/info/repository/DW_Auth_Mng_Sql.groovy
@@ -0,0 +1,20 @@
+package com.example.TestRest.info.repository
+
+class DW_Auth_Mng_Sql {
+	public static String SELECT = """""";
+	
+	public static SELECT_QUERY(String lang, String userid, String username)
+	{
+//		ALTER PROC [dbo].[ZZ_USP_AUTH_USER_SEARCH]
+//			@I_LANG					VARCHAR(2),				-- 언어(KR)
+//			@I_USERID           	VARCHAR(20),			-- 사용자아이디
+//			@I_USERNAME				VARCHAR(50)				-- 사용자이름
+//		AS
+		
+		SELECT = """""";
+		SELECT += """ EXEC ZZ_USP_AUTH_USER_SEARCH """
+		SELECT += """  '""" + lang + """'"""
+		SELECT += """, '""" + userid + """'"""
+		SELECT += """, '""" + username + """'"""
+	}
+}
