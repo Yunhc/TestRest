1747428bf16640a750c8cfb416a80acab2103f3f
Yunhc <yunhc@wavelink.co.kr>
2022-02-22 08:57:41 +0900

    Update DW_Auth_Mng_Service.java

--- src/main/java/com/example/TestRest/info/DW_Auth_Mng_Service.java
+++ src/main/java/com/example/TestRest/info/DW_Auth_Mng_Service.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Service;
 
 import com.example.TestRest.info.model.DW_Auth_Mng_Auth_Res_Param;
 import com.example.TestRest.info.model.DW_Auth_Mng_User_Res_Param;
+import com.example.TestRest.info.model.ReturnMsg;
 import com.example.TestRest.info.repository.DW_Auth_Mng_Repository;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class DW_Auth_Mng_Service {
 	private final DW_Auth_Mng_Repository dw_repository;
@@ -25,5 +28,10 @@ public class DW_Auth_Mng_Service {
 	//사용자 조회
 	public List<DW_Auth_Mng_Auth_Res_Param> findAuth(String req_param) {
 		return this.dw_repository.findAuth(req_param);
+	}
+	
+	public List<ReturnMsg> saveList(String req_param) {
+		log.debug("/saveList = {}", req_param.toString());
+		return this.dw_repository.saveList(req_param);
 	}
 }
