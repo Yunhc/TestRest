ebcc782c64a1106ba872e337872611ecfad5e528
Yunhc <yunhc@wavelink.co.kr>
2022-02-22 10:08:22 +0900

    Create DW_Return_Message.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Return_Message.java
@@ -0,0 +1,11 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Return_Message {
+	public String code;
+	public String message;
+}
