31f8428c5827e141ced83bcf2e63adf2b6840514
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-24 11:44:37 +0900

    Create DW_STC_PDA_Scan_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_STC_PDA_Scan_Req_Param.java
@@ -0,0 +1,17 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_STC_PDA_Scan_Req_Param {
+	public String werks;
+	public String lgort;
+	public String barno;
+	public int qty;
+	public String loc;
+	public String userid;
+	public String force;
+	public String online;
+}
