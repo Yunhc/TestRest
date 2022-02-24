4afa437a710a6d844a5ac7358cd3719ffdb3cb3e
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-24 11:44:43 +0900

    Create DW_STC_PDA_Search_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_STC_PDA_Search_Req_Param.java
@@ -0,0 +1,13 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_STC_PDA_Search_Req_Param {
+	public String i_lang;
+	public String i_werks;
+	public String i_userid;
+	public String i_lgort;
+}
