d99f5d6ab7b5a13523e77241953c5b5fd8980662
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-24 11:44:46 +0900

    Create DW_STC_PDA_Search_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_STC_PDA_Search_Res_Param.java
@@ -0,0 +1,16 @@
+
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_STC_PDA_Search_Res_Param {
+	public String stc_no;
+	public String silsatype;
+	public String matgroup;
+	public String matnr;
+	public String code;
+	public String message;
+}
