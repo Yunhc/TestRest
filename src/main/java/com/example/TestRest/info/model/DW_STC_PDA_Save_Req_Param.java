79139a1026ca7fc22d50a925ba6d05f85ff650a7
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-25 18:23:55 +0900

    Create DW_STC_PDA_Save_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_STC_PDA_Save_Req_Param.java
@@ -0,0 +1,18 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_STC_PDA_Save_Req_Param {
+	public String i_lang;
+	public String i_werks;
+	public String i_userid;
+	public String i_lgort;
+	public String i_barno;
+	public String i_qty;
+	public String i_loc;
+	public String i_packtype;
+	public String i_area;
+}
