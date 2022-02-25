ef98c504524fdc7370060ea92f8d7b2195cfc63a
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-25 18:24:01 +0900

    Update DW_STC_PDA_Sql.groovy

--- src/main/java/com/example/TestRest/info/repository/DW_STC_PDA_Sql.groovy
+++ src/main/java/com/example/TestRest/info/repository/DW_STC_PDA_Sql.groovy
@@ -1,6 +1,7 @@
 package com.example.TestRest.info.repository
 
 import com.example.TestRest.info.model.DW_STC_PDA_Search_Req_Param
+import com.example.TestRest.info.model.DW_STC_PDA_Save_Req_Param
 import com.example.TestRest.info.model.DW_STC_PDA_Scan_Req_Param
 
 class DW_STC_PDA_Sql {
@@ -30,5 +31,21 @@ class DW_STC_PDA_Sql {
 		SELECT += """, '""" + req_param.userid + """'"""
 		SELECT += """, '""" + req_param.force + """'"""
 		SELECT += """, '""" + req_param.online + """'"""
+	}
+
+	//오프라인 재고실사 저장
+	public static SAVE_QUERY(DW_STC_PDA_Save_Req_Param req_param)
+	{
+		SELECT = """""";
+		SELECT += """ EXEC USP_MWMS2_STOCK_COUNT_SAVE """
+		SELECT += """  '""" + req_param.i_lang + """'"""
+		SELECT += """, '""" + req_param.i_werks+ """'"""
+		SELECT += """, '""" + req_param.i_userid + """'"""
+		SELECT += """, '""" + req_param.i_lgort + """'"""
+		SELECT += """, '""" + req_param.i_barno + """'"""
+		SELECT += """, '""" + req_param.i_qty + """'"""
+		SELECT += """, '""" + req_param.i_loc + """'"""
+		SELECT += """, '""" + req_param.i_packtype + """'"""
+		SELECT += """, '""" + req_param.i_area + """'"""
 	}
 }
