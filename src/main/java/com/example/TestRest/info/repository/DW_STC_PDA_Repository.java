dbfe00248588ca3f5d94a5e1eff11bd873be5517
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-02-25 18:23:58 +0900

    Update DW_STC_PDA_Repository.java

--- src/main/java/com/example/TestRest/info/repository/DW_STC_PDA_Repository.java
+++ src/main/java/com/example/TestRest/info/repository/DW_STC_PDA_Repository.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 
 import com.example.TestRest.info.model.DW_STC_PDA_Search_Req_Param;
 import com.example.TestRest.info.model.DW_STC_PDA_Search_Res_Param;
+import com.example.TestRest.info.model.DW_Return_Message;
+import com.example.TestRest.info.model.DW_STC_PDA_Save_Req_Param;
 import com.example.TestRest.info.model.DW_STC_PDA_Scan_Req_Param;
 import com.example.TestRest.info.model.DW_STC_PDA_Scan_Res_Param;
 
@@ -45,4 +47,15 @@ public class DW_STC_PDA_Repository {
 		List<DW_STC_PDA_Scan_Res_Param> res = this.jdbcTemplate.query(DW_STC_PDA_Sql.SELECT, BeanPropertyRowMapper.newInstance(DW_STC_PDA_Scan_Res_Param.class));
 		return res;
 	}
+	
+	//오프라인 재고실사 저장
+	public List<DW_Return_Message> saveList(DW_STC_PDA_Save_Req_Param req_param){
+		
+		DW_STC_PDA_Sql.SAVE_QUERY(req_param );
+		log.debug("saveList query = {}", DW_STC_PDA_Sql.SELECT);
+
+		
+		List<DW_Return_Message> res = this.jdbcTemplate.query(DW_STC_PDA_Sql.SELECT, BeanPropertyRowMapper.newInstance(DW_Return_Message.class));
+		return res;
+	}	
 }
