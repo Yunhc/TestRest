e2fc268486a22f9c1f88b80b602bf85e0f5974aa
Yunhc <yunhc@wavelink.co.kr>
2022-03-07 10:14:35 +0900

    Create DW_Stock_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Stock_Res_Param.java
@@ -0,0 +1,81 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Stock_Res_Param {
+//	public String SEL;	
+//	public String WERKS;	
+//	public String WERKSNM;	
+//	public String STATUS2;	
+//	public String LGORT;	
+//	public String LGORTNM;	
+//	public String CUSTOMER;	
+//	public String MATNR;	
+//	public String MAKTX;	
+//	public String WTSCHK;	
+//	public String QTY;	
+//	public String MEINS;	
+//	public String UQTY;	
+//	public String UMEINS;	
+//	public String PREQTY;	
+//	public String PREMEINS;	
+//	public String PREUQTY;	
+//	public String PREUMEINS;	
+//	public String QTY_N;	
+//	public String MEINS_N;	
+//	public String UQTY_N;	
+//	public String UMEINS_N;	
+//	public String QTY_C;	
+//	public String MEINS_C;	
+//	public String UQTY_C;	
+//	public String UMEINS_C;	
+//	public String STATUS;	
+//	public String LPM_FO;	
+//	public String LPM_FI;	
+//	public String LPM_BO;	
+//	public String LPM_BI;	
+//	public String PLATE_F;	
+//	public String PLATE_B;
+//	public String CODE;
+//	public String MESSAGE;
+	
+	
+	public String sel;	
+	public String werks;	
+	public String werksnm;	
+	public String status2;	
+	public String lgort;	
+	public String lgortnm;	
+	public String customer;	
+	public String matnr;	
+	public String maktx;	
+	public String wtschk;	
+	public double qty;	
+	public String meins;	
+	public double uqty;	
+	public String umeins;	
+	public String preqty;	
+	public String premeins;	
+	public String preuqty;	
+	public String preumeins;	
+	public String qty_n;	
+	public String meins_n;	
+	public String uqty_n;	
+	public String umeins_n;	
+	public String qty_c;	
+	public String meins_c;	
+	public String uqty_c;	
+	public String umeins_c;	
+	public String status;	
+	public String lpm_fo;	
+	public String lpm_fi;	
+	public String lpm_bo;	
+	public String lpm_bi;	
+	public String plate_f;	
+	public String plate_b;
+	public String code;
+	public String message;
+}
