7786a0e21d51b078f23307820e686074755b676c
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-07 21:35:15 +0900

    Create DW_Autolabeller_PDA_Search_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Autolabeller_PDA_Search_Res_Param.java
@@ -0,0 +1,15 @@
+
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Autolabeller_PDA_Search_Res_Param {
+	public String maktx;
+	public String procqty;
+	
+	public String code;
+	public String message;
+}
