67735662791d2142c67ce910fa3d097f4ed38cbf
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-07 21:35:06 +0900

    Create DW_Autolabeller_PDA_Service.java

---
+++ src/main/java/com/example/TestRest/info/DW_Autolabeller_PDA_Service.java
@@ -0,0 +1,33 @@
+package com.example.TestRest.info;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+//import com.example.TestRest.info.model.DW_Return_Message;
+//import com.example.TestRest.info.model.DW_STC_Autolabeller_Save_Req_Param;
+import com.example.TestRest.info.model.DW_Autolabeller_PDA_Search_Req_Param;
+import com.example.TestRest.info.model.DW_Autolabeller_PDA_Search_Res_Param;
+import com.example.TestRest.info.repository.DW_Autolabeller_PDA_Repository;
+
+
+@Service
+
+public class DW_Autolabeller_PDA_Service {
+	private final DW_Autolabeller_PDA_Repository dw_repository;
+	
+	public DW_Autolabeller_PDA_Service(DW_Autolabeller_PDA_Repository dw_repository) {
+		this.dw_repository = dw_repository;
+	}
+	
+	//재고실사 정보 조회
+	public List<DW_Autolabeller_PDA_Search_Res_Param> search_List(DW_Autolabeller_PDA_Search_Req_Param req_param) {
+		return this.dw_repository.searchList(req_param);
+	}
+
+
+//	//오프라인 재고실사 저장
+//	public List<DW_Return_Message> save_List(DW_Autolabeller_PDA_Save_Req_Param req_param) {
+//		return this.dw_repository.saveList(req_param);
+//	}
+}
