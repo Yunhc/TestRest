d69fad0ae95ab9a5c8c5a73596e175bea8f80e44
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-11 19:28:09 +0900

    Update DW_Autolabeller_PDA_Repository.java

--- src/main/java/com/example/TestRest/info/repository/DW_Autolabeller_PDA_Repository.java
+++ src/main/java/com/example/TestRest/info/repository/DW_Autolabeller_PDA_Repository.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Repository;
 
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Search_Req_Param;
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Search_Res_Param;
-//import com.example.TestRest.info.model.DW_Return_Message;
-//import com.example.TestRest.info.model.DW_Autolabeller_PDA_Save_Req_Param;
+import com.example.TestRest.info.model.DW_Return_Message;
+import com.example.TestRest.info.model.DW_Autolabeller_PDA_Save_Req_Param;
 
 
 import lombok.extern.slf4j.Slf4j;
@@ -38,14 +38,14 @@ public class DW_Autolabeller_PDA_Repository {
 
 
 	
-//	//오프라인 재고실사 저장
-//	public List<DW_Return_Message> saveList(DW_Autolabeller_PDA_Save_Req_Param req_param){
-//		
-//		DW_Autolabeller_PDA_Sql.SAVE_QUERY(req_param );
-//		log.debug("saveList query = {}", DW_Autolabeller_PDA_Sql.SELECT);
-//
-//		
-//		List<DW_Return_Message> res = this.jdbcTemplate.query(DW_Autolabeller_PDA_Sql.SELECT, BeanPropertyRowMapper.newInstance(DW_Return_Message.class));
-//		return res;
-//	}	
+	//생산실적 처리
+	public List<DW_Return_Message> saveList(DW_Autolabeller_PDA_Save_Req_Param req_param){
+		
+		DW_Autolabeller_PDA_Sql.SAVE_QUERY(req_param );
+		log.debug("saveList query = {}", DW_Autolabeller_PDA_Sql.SELECT);
+
+		
+		List<DW_Return_Message> res = this.jdbcTemplate.query(DW_Autolabeller_PDA_Sql.SELECT, BeanPropertyRowMapper.newInstance(DW_Return_Message.class));
+		return res;
+	}	
 }
