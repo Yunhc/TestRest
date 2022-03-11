d2f513146b6410d918da43a5e1562d31d6995a5d
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-11 19:28:12 +0900

    Update DW_Autolabeller_PDA_Sql.groovy

--- src/main/java/com/example/TestRest/info/repository/DW_Autolabeller_PDA_Sql.groovy
+++ src/main/java/com/example/TestRest/info/repository/DW_Autolabeller_PDA_Sql.groovy
@@ -1,7 +1,7 @@
 package com.example.TestRest.info.repository
 
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Search_Req_Param
-//import com.example.TestRest.info.model.DW_Autolabeller_PDA_Save_Req_Param
+import com.example.TestRest.info.model.DW_Autolabeller_PDA_Save_Req_Param
 
 
 class DW_Autolabeller_PDA_Sql {
@@ -19,19 +19,20 @@ class DW_Autolabeller_PDA_Sql {
 	}
 
 
-	//오프라인 재고실사 저장
-//	public static SAVE_QUERY(DW_Autolabeller_PDA_Save_Req_Param req_param)
-//	{
-//		SELECT = """""";
-//		SELECT += """ EXEC USP_MWMS2_STOCK_COUNT_SAVE """
-//		SELECT += """  '""" + req_param.i_lang + """'"""
-//		SELECT += """, '""" + req_param.i_werks+ """'"""
-//		SELECT += """, '""" + req_param.i_userid + """'"""
-//		SELECT += """, '""" + req_param.i_lgort + """'"""
-//		SELECT += """, '""" + req_param.i_barno + """'"""
-//		SELECT += """, '""" + req_param.i_qty + """'"""
-//		SELECT += """, '""" + req_param.i_loc + """'"""
-//		SELECT += """, '""" + req_param.i_packtype + """'"""
-//		SELECT += """, '""" + req_param.i_area + """'"""
-//	}
+	//생산실적 처리
+	public static SAVE_QUERY(DW_Autolabeller_PDA_Save_Req_Param req_param)
+	{
+		SELECT = """""";
+		SELECT += """ EXEC USP_MWMS2_AUTOLABELLER_PO_SAVE """
+		SELECT += """  '""" + req_param.i_lang + """'"""
+		SELECT += """, '""" + req_param.i_werks+ """'"""
+		SELECT += """, '""" + req_param.i_userid + """'"""
+		SELECT += """, '""" + req_param.i_ord_no + """'"""
+		SELECT += """, '""" + req_param.i_grpbarno + """'"""
+		SELECT += """, '""" + req_param.i_date_hdf + """'"""
+		SELECT += """, '""" + req_param.i_date_fmfm + """'"""
+		SELECT += """, '""" + req_param.i_date_prod + """'"""
+		SELECT += """, '""" + req_param.i_lotno + """'"""
+		SELECT += """, '""" + req_param.i_barno + """'"""
+	}
 }
