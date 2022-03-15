966c0c01db6caf1f46aa55b28871d94fcc46c9d9
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-15 13:34:54 +0900

    Create DW_Autolabeller_PDA_Change_Lot_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Autolabeller_PDA_Change_Lot_Res_Param.java
@@ -0,0 +1,38 @@
+
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Autolabeller_PDA_Change_Lot_Res_Param {
+	public String barno;
+	public String proctype;
+	public String lbltype;
+	public String grpbarno;
+	public String subflag;
+	public String werks;
+	public String arbpl;
+	public String lgort;
+	public String matnr;
+	public String meins;
+	public String qty;
+	public String orderno;
+	public String aufnr;
+	public String plnum;
+	public String status;
+	public String lotno;
+	public String proddate;
+	public String prodtime;
+	public String prodcldate;
+	public String indate;
+	public String intime;
+	public String incldate;
+	public String maktx;
+	public String boxcnt;
+	public String boxtotcnt;
+	
+	public String code;
+	public String message;
+}
