901c0543310c55977094ba074d52a9d231608a60
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-15 13:34:46 +0900

    Update DW_Autolabeller_PDA_Controller.java

--- src/main/java/com/example/TestRest/info/DW_Autolabeller_PDA_Controller.java
+++ src/main/java/com/example/TestRest/info/DW_Autolabeller_PDA_Controller.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Search_Res_Param;
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Search_Req_Param;
+import com.example.TestRest.info.model.DW_Autolabeller_PDA_Change_Lot_Req_Param;
+import com.example.TestRest.info.model.DW_Autolabeller_PDA_Change_Lot_Res_Param;
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Save_Req_Param;
 import com.example.TestRest.info.model.DW_Return_Message;
 
@@ -28,7 +30,7 @@ public class DW_Autolabeller_PDA_Controller {
 		this.dw_Service = dw_Service;
 	}
 
-	
+	//오토라벨러 생산입고 화면
 	//생산오더 정보 조회
 	@CrossOrigin("*")
 	@PostMapping(value="/dw/autolabeller/pda/search")
@@ -41,7 +43,6 @@ public class DW_Autolabeller_PDA_Controller {
 		return res;
 	}
 	
-
 	//생산실적 처리
 	@CrossOrigin("*")
 	@PostMapping(value="/dw/autolabeller/pda/save")
@@ -50,6 +51,20 @@ public class DW_Autolabeller_PDA_Controller {
 		log.debug("/dw/autolabeller/pda/save");
 		log.debug("/dw/autolabeller/pda/save request = {}", req_param.toString());
 		List<DW_Return_Message> res = dw_Service.save_List(req_param);
+		return res;
+	}
+	
+	
+	//오토라벨러 Lot 번호 일괄변경 화면
+	//바코드 정보 조회 및 Lot번호 변경
+	@CrossOrigin("*")
+	@PostMapping(value="/dw/autolabeller/pda/change_lot")
+	@ResponseStatus(value = HttpStatus.OK)
+	public Object dw_autolabeller_pda_change_lot(@RequestBody DW_Autolabeller_PDA_Change_Lot_Req_Param req_param) {
+		
+		log.debug("/dw/autolabeller/pda/change_lot");
+		log.debug("/dw/autolabeller/pda/change_lot request = {}", req_param.toString());
+		List<DW_Autolabeller_PDA_Change_Lot_Res_Param> res = dw_Service.change_List(req_param);
 		return res;
 	}
 }
