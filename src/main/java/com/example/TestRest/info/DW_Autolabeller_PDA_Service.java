e069c6340ad9a5cff0a676c8d8b0a166484c7518
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-22 16:00:54 +0900

    Update DW_Autolabeller_PDA_Service.java

--- src/main/java/com/example/TestRest/info/DW_Autolabeller_PDA_Service.java
+++ src/main/java/com/example/TestRest/info/DW_Autolabeller_PDA_Service.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.example.TestRest.info.model.DW_Return_Message;
+import com.example.TestRest.info.model.DW_Autolabeller_PDA_Barcode_Info_Req_Param;
+import com.example.TestRest.info.model.DW_Autolabeller_PDA_Barcode_Info_Res_Param;
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Change_Lot_Req_Param;
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Change_Lot_Res_Param;
 import com.example.TestRest.info.model.DW_Autolabeller_PDA_Save_Req_Param;
@@ -38,5 +40,11 @@ public class DW_Autolabeller_PDA_Service {
 	//바코드 정보 조회 및 Lot번호 변경
 	public List<DW_Autolabeller_PDA_Change_Lot_Res_Param> change_List(DW_Autolabeller_PDA_Change_Lot_Req_Param req_param) {
 		return this.dw_repository.changeList(req_param);
+	}	
+
+	//오토라벨러 바코드 정보조회 화면
+	//바코드 정보 조회 및 낱바코드 리스트 조회
+	public List<DW_Autolabeller_PDA_Barcode_Info_Res_Param> barcode_List(DW_Autolabeller_PDA_Barcode_Info_Req_Param req_param) {
+		return this.dw_repository.barcodeList(req_param);
 	}	
 }
