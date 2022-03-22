f687fdcc69862502d0d10d9cd6a5d8c814a5942a
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-22 16:01:15 +0900

    Create DW_Autolabeller_PDA_Barcode_Info_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Autolabeller_PDA_Barcode_Info_Res_Param.java
@@ -0,0 +1,40 @@
+
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Autolabeller_PDA_Barcode_Info_Res_Param {
+	public String seq;
+	public String barno;
+	public String qty;
+	public String incldate;
+	public String lotno;
+	
+	public String proctype;
+	public String lbltype;
+	public String grpbarno;
+	public String subflag;
+	public String werks;
+	public String arbpl;
+	public String lgort;
+	public String matnr;
+	public String meins;
+	public String orderno;
+	public String aufnr;
+	public String plnum;
+	public String status;
+	public String proddate;
+	public String prodtime;
+	public String prodcldate;
+	public String indate;
+	public String intime;
+	public String maktx;
+	public String boxcnt;
+	public String boxtotcnt;
+		
+	public String code;
+	public String message;
+}
