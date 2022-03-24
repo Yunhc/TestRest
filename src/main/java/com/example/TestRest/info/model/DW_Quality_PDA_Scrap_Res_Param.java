45e1206cb42feb6afaba96c4207bb7997790b280
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-24 17:59:29 +0900

    Create DW_Quality_PDA_Scrap_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Quality_PDA_Scrap_Res_Param.java
@@ -0,0 +1,41 @@
+
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Quality_PDA_Scrap_Res_Param {
+	public String sel;
+	public String barno;
+	public String status;
+	public String proctype;
+	public String lbltype;
+	public String qty;
+	public String meins;
+	public String matnr;
+	public String maktx;
+	public String grpbarno;
+	public String subflag;
+	public String werks;
+	public String arbpl;
+	public String lgort;
+	public String kdauf;
+	public String kdpos;
+	public String prebar;
+	public String prematnr;
+	public String vbeln;
+	public String posnr;
+	public String lotno;
+	public String proddate;
+	public String lpm_fo;
+	public String lpm_fi;
+	public String lpm_bo;
+	public String lpm_bi;
+	public String plate_f;
+	public String plate_b;
+		
+	public String code;
+	public String message;
+}
