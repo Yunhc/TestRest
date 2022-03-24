0a0e82091f471d8b8b03983e56d65021e87888d4
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-24 17:59:27 +0900

    Create DW_Quality_PDA_Scrap_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Quality_PDA_Scrap_Req_Param.java
@@ -0,0 +1,14 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Quality_PDA_Scrap_Req_Param {
+	public String i_lang;
+	public String i_werks;
+	public String i_userid;
+	public String i_barno;
+	public String i_calltype;
+}
