a15e488ee846d50d69410a1d2b86c8ada723e291
wavelink <yunhc@wavelink.co.kr>
2022-04-04 15:38:49 +0900

    Create DW_Master_Company_Save_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Master_Company_Save_Res_Param.java
@@ -0,0 +1,19 @@
+
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Master_Company_Save_Res_Param {
+	public String sel;
+	public String bukrs;
+	public String comname;
+	public String useflag;
+	public String upduser;
+	public String upddate;
+	
+	public String code;
+	public String message;
+}
