7568f9cf643e42e086a91742db6e37a84e125650
wavelink <yunhc@wavelink.co.kr>
2022-04-04 15:38:46 +0900

    Create DW_Master_Company_Save_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Master_Company_Save_Req_Param.java
@@ -0,0 +1,19 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Master_Company_Save_Req_Param {
+	public String i_lang;
+	public String i_werks;
+	public String i_userid;
+	public String i_ord_no;
+	public String i_grpbarno;
+	public String i_date_hdf;
+	public String i_date_fmfm;
+	public String i_date_prod;
+	public String i_lotno;
+	public String i_barno;
+}
