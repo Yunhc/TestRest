cbd15fb7b8cc200fe037aa868e94acbd1cede17a
wavelink <yunhc@wavelink.co.kr>
2022-04-04 15:39:05 +0900

    Create DW_Master_Company_Search_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Master_Company_Search_Req_Param.java
@@ -0,0 +1,13 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Master_Company_Search_Req_Param {
+	public String i_lang;
+	public String i_bukrs;
+	public String i_companynm;
+	public String i_useflag;
+}
