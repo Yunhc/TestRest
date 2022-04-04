682e8926e6a61166d60701d5bfadfbb414779ed9
wavelink <yunhc@wavelink.co.kr>
2022-04-04 15:39:07 +0900

    Create DW_Master_Company_Search_Res_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Master_Company_Search_Res_Param.java
@@ -0,0 +1,17 @@
+
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Master_Company_Search_Res_Param {
+	public String sel;
+	public String bukrs;
+	public String comname;
+	public String useflag;
+	public String upduser;
+	public String upddate;
+
+}
