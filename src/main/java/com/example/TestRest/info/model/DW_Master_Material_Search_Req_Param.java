7eb3215c0810e45fb92db9af3f6ec17207d24c2a
wavelink <yunhc@wavelink.co.kr>
2022-04-04 15:39:10 +0900

    Create DW_Master_Material_Search_Req_Param.java

---
+++ src/main/java/com/example/TestRest/info/model/DW_Master_Material_Search_Req_Param.java
@@ -0,0 +1,14 @@
+package com.example.TestRest.info.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DW_Master_Material_Search_Req_Param {
+	public String lang;
+	public String matnr;
+	public String maktx;
+	public String mtart;	
+	public String useflag;
+}
